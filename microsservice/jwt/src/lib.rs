2ae057b24080fdcc2cafc1c086e1a315bd50b365
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-29 22:07:29 -0300

    [ :passport_control: ] | create: jwt - encode  (lib::jwt)

--- microsservice/jwt/src/lib.rs
+++ microsservice/jwt/src/lib.rs
@@ -1,1 +1,20 @@
 pub mod model;
+
+use crate::model::{claims::Claims, user::User};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{encode, EncodingKey, Header};
+
+pub fn encode_jwt(user: User) -> Result<String, String> {
+    let claims = Claims {
+        email: user.email,
+        exp: (Utc::now() + Duration::days(1)).timestamp(),
+    };
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret("mykey".as_bytes()),
+    )
+    .map_err(|e| e.to_string());
+
+    return token;
+}
